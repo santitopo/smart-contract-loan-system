70f9a0443557c9d5e4ec6813a550f47beccc7ec3
santitopo <santiago.topolansky@decemberlabs.com>
2022-06-01 23:43:10 -0300

    Changes made in data structures and observations to some methods

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -8,7 +8,7 @@ import "./ERC721Receiver.sol";
 contract LoanContract is Owneable, ERC721Receiver {
     uint256 public identifier = 1;
     uint256 public loanAmount;
-    uint256 public maxTimeInSeconds;
+    address public nftContact;
     mapping(uint256 => Loan) public loans;
 
     struct Loan {
@@ -18,27 +18,39 @@ contract LoanContract is Owneable, ERC721Receiver {
         // Que sería "balance del deudor"?
         uint256 currentDebt;
         uint256 dueDate;
-        uint256 totalAmount;
+        uint256 loanAmount;
+        uint256 interestPercentage;
+        LoanStatus status;
     }
 
     enum LoanStatus { Pending, Approved, Rejected, Paid }
 
-    constructor() payable {
+    constructor(address _nftContact) payable {
         owner = msg.sender;
+        nftContact = _nftContact;
     }
 
     function requestLoan(uint256 token_id) external {
+        //Posible flujo
 
+        // Paso 1: Validar contra el nftContract que el msg.sender sea el dueño de ese NFT
+        //          y si lo es, crear el Loan en estado Pending.
+        // Paso 2: El owner agrega el interés, monto prestado, y fecha de vencimiento al loan.
+        // Paso 3: Si el usuario quiere aceptar el loan, puede hacerlo transfiriéndo el NFT a la address de el LoanContract 
+        // Paso 4: El owner luego aprueba entonces el préstamo con un método approveLoan(id) y se le transfiere el dinero a la address que lo solicitó
     }
 
+    //Debería de recibir el id del loan para setear el amount
     function setLoanAmount(uint256 _loanAmount) external {
         loanAmount = _loanAmount;
     }
 
+    //No recibe id porque asume que es el único Loan que tiene disponible esta address
     function getLoanStatus() external view returns(string memory) {
 
     }
 
+    //No recibe id porque asume que es el único Loan que tiene disponible esta address
     function withdrawLoanAmount() external {
 
     }
@@ -48,6 +60,7 @@ contract LoanContract is Owneable, ERC721Receiver {
 
     }
 
+    //Debería de recibir el id del loan para setear el interest
     function setInterest(uint8 _interestPercentage) external {
 
     }
@@ -64,8 +77,8 @@ contract LoanContract is Owneable, ERC721Receiver {
 
     }
 
+    //Debería de recibir el id del loan para setear el deadline
     function setDeadline(uint256 _maxTime) external {
-        maxTimeInSeconds = _maxTime;
     }
 
     function getDeadline() external view returns(uint256 _maxTime) {
